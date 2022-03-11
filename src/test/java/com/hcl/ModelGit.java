aeca1c780a9f668dc518b716b37e2b5d0fcd6472
Dhanasekaran5599 <dhanasekaran99022@gmail.com>
2022-03-11 22:51:24 +0530

    message

---
+++ src/test/java/com/hcl/ModelGit.java
@@ -0,0 +1,23 @@
+package com.hcl;
+
+public class ModelGit {
+	private void method5() {
+		System.out.println("method5");
+
+	}
+
+	private void method6() {
+		System.out.println("method6");
+
+	}
+	
+	private void method7() {
+		System.out.println("method7");
+
+	}
+	
+	private void method8() {
+		System.out.println("method8");
+
+	}
+}
