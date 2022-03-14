5013d67f0d42f3d86941528117e947cda25b195b
javeedfazul <javeed.tntj@gmail.com>
2022-03-14 11:52:25 +0530

    newcode

--- src/test/java/com/hcl/ModelGit.java
+++ src/test/java/com/hcl/ModelGit.java
@@ -20,4 +20,19 @@ public class ModelGit {
 		System.out.println("method8");
 
 	}
+
+	private void method1() {
+		System.out.println("m1");
+
+	}
+
+	private void method2() {
+		System.out.println("m2");
+
+	}
+
+	private void method3() {
+		System.out.println("m3");
+	}
+
 }
